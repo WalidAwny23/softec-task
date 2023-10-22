7ce1b7fe2d216b81139cc84d37fdcd8bbd0902bb
Walid Awny <walid.awny@yahoo.com>
2023-10-22 23:27:33 +0300

    adding order details page

--- src/app/feature_modules/orders/orders-routing.module.ts
+++ src/app/feature_modules/orders/orders-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { OrdersComponent } from './orders.component';
+import { OrderDetailsComponent } from './order-details/order-details.component';
 
 const routes: Routes = [
   { path: '', component: OrdersComponent },
+  { path: 'order-details/:id', component: OrderDetailsComponent },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
