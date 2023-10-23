67283f45b01a5ad2d8a9acf9defba82252855a96
Walid Awny <walid.awny@yahoo.com>
2023-10-23 20:23:08 +0300

    order details page

--- src/app/feature_modules/orders/orders.component.ts
+++ src/app/feature_modules/orders/orders.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-orders',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./orders.component.scss']
 })
 export class OrdersComponent {
+  constructor(private router: Router){
 
+  }
+
+  openOrder(){
+    this.router.navigate(['layout/orders/order-details/1']);
+  }
 }
